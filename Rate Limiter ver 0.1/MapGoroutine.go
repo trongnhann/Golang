43c3a6a23a593db4c59c3942d7297bb02e131de0
trongnhann <44260462+trongnhann@users.noreply.github.com>
2018-11-14 17:51:32 +0700

    Update MapGoroutine.go

--- Rate Limiter ver 0.1/MapGoroutine.go
+++ Rate Limiter ver 0.1/MapGoroutine.go
@@ -20,29 +20,29 @@ func NewMapGoroutine() *MapGoroutine {
 
 // Load is load value of key
 func (rm *MapGoroutine) Load(key interface{}) (value interface{}, ok bool) {
-//	rm.RLock()
-//	defer rm.RUnlock()
+	rm.RLock()
+	defer rm.RUnlock()
 	value, ok = rm.List[key]
 	return
 }
 
 // Delete is remove key, value
 func (rm *MapGoroutine) Delete(key interface{}) {
-//	rm.Lock()
-//	defer rm.Unlock()
+	rm.Lock()
+	defer rm.Unlock()
 	delete(rm.List, key)
 }
 
 // Store is add key, value
 func (rm *MapGoroutine) Store(key interface{}, value interface{}) {
-//	rm.Lock()
-//	defer rm.Unlock()
+	rm.Lock()
+	defer rm.Unlock()
 	rm.List[key] = value
 }
 
 // Len is get length of map
 func (rm *MapGoroutine) Len() uint32 {
-//	rm.Lock()
-//	defer rm.Unlock()
+	rm.Lock()
+	defer rm.Unlock()
 	return uint32(len(rm.List))
 }
